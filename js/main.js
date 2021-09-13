0e3427f7b7ef02c1b655f4aee154b467de1c905a
Janek_Janiak <janek.janiak@gmail.com>
2021-09-13 09:15:30 +0200

    Feature/Add Reading class in main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+class Reading {
+  constructor(id, dateCreated, date, time, systolic, diastolic, heartRate, stress) {
+    this.id = id;
+    this.dateCreated = dateCreated;
+    this.date = date;
+    this.time = time;
+    this.systolic = systolic;
+    this.diastolic = diastolic;
+    this.heartRate = heartRate;
+    this.stress = stress;
+  }
+}
+
+let newReading = new Reading(1, 1614866400000, "2021-03-04", "15:00", 120, 80, 82, 1,);
+
+console.log(newReading);
+
+
